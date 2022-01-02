4444e7c683de1ba104465d4127195556529e1d8c
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-01 23:33:10 -0300

    Update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - L6
                 </div>
 
                 <div class="links">
