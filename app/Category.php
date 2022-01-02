9b310c35d782c7cb9fa34debde374dca8e48f03f
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-01 22:01:29 -0300

    Create products method in Category model

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Return relationship with Products
+     *
+     * @return Object
+     */
+    public function products(): Object
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
