915b993720df9220a016aa30a79a2c5fc14a75d6
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-01 22:39:01 -0300

    Adding fillable attribute in Category model

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'description', 'slug'];
+
     /**
      * Return relationship with Products
      *
