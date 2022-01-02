86c28611f5c36fd417ee294a32a5d9a0d27a8dce
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-01 22:01:03 -0300

    Create categories method in Product model

--- app/Product.php
+++ app/Product.php
@@ -15,4 +15,14 @@ class Product extends Model
     {
         return $this->belongsTo(Store::class);
     }
+
+    /**
+     * Return relationship with Categories
+     *
+     * @return Object
+     */
+    public function categories(): Object
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
