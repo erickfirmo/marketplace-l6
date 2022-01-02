dcdaa6ef366d1003b58ee428831772c5249b34fe
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-01 22:39:41 -0300

    Adding fillable attribute in Product model

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'description', 'body', 'price', 'slug'];
+
     /**
      * Return relationship with Store
      *
