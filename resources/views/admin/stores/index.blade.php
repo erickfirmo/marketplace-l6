1c1dbf1446a3f7b543deba62d5114c233acba440
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-03 00:35:57 -0300

    Create stores list

---
+++ resources/views/admin/stores/index.blade.php
@@ -0,0 +1,24 @@
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Loja</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($stores as $store)
+            <tr>
+                <td>{{ $store->id }}</td>
+                <td>{{ $store->name}}</td>
+                <td></td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="3"></td>
+            </tr>
+        @endforelse
+    </tbody>
+</table>
+
+{{ $stores->links() }}
