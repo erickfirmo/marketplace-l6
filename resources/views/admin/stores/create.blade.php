178e610b0625674b80b5a31e179614a69e11ba87
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-25 23:56:26 -0300

    Create store form

---
+++ resources/views/admin/stores/create.blade.php
@@ -0,0 +1,37 @@
+<h1>Criar Loja</h1>
+<form action="{{ route('admin.stores.store') }}" method="post">
+    @csrf
+    <div>
+        <label for="name">Nome da Loja</label>
+        <input id="name" type="text" name="name">
+    </div>
+
+    <div>
+        <label for="description">Descrição</label>
+        <input id="description" type="text" name="descripton">
+    </div>
+
+    <div>
+        <label for="phone">Telefone</label>
+        <input id="phone" type="text" name="phone">
+    </div>
+    <div>
+        <label for="mobile_phone">Celular/Whatsapp</label>
+        <input id="mobile_phone" type="text" name="mobile_phone">
+    </div>
+    <div>
+        <label for="slug">Slug</label>
+        <input id="slug" type="text" name="slug">
+    </div>
+    <div>
+        <label for="user">Usuário</label>
+        <select name="user" id="user">
+            @foreach ($users as $user)
+                <option value="{{ $user->id }}">{{ $user->name }} </option>
+            @endforeach
+        </select>
+    </div>
+    <div>
+        <button type="submit">Criar</button>
+    </div>
+</form>
