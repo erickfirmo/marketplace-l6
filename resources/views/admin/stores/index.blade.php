a27984838e4d72f105988da4ca022326e7728710
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-26 20:53:53 -0300

    Update stores list

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,24 +1,28 @@
-<table>
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Loja</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($stores as $store)
-            <tr>
-                <td>{{ $store->id }}</td>
-                <td>{{ $store->name}}</td>
-                <td></td>
-            </tr>
-        @empty
-            <tr>
-                <td colspan="3"></td>
-            </tr>
-        @endforelse
-    </tbody>
-</table>
+@extends('layouts.app')
 
-{{ $stores->links() }}
+@section('content')
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Loja</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($stores as $store)
+                <tr>
+                    <td>{{ $store->id }}</td>
+                    <td>{{ $store->name}}</td>
+                    <td></td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3"></td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{ $stores->links() }}
+@endsection
