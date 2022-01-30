2e328f7e6c1eccfa305280900f45bd7fa8cefca0
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-30 00:42:33 -0300

    Create constructor in ProductController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+    
     /**
      * Display a listing of the resource.
      *
