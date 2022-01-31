d6fde1c353a829f46f7158f2880024fbc774abb9
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-30 23:55:16 -0300

    Update stores routes

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,6 @@ Route::get('/', function () {
 });
 
 Route::prefix('admin')->namespace('Admin')->name('admin.')->group(function() {
-    Route::prefix('/stores')->name('stores.')->group(function() {
-        Route::get('/', 'StoreController@index')->name('index');
-        Route::get('/create', 'StoreController@create')->name('create');
-        Route::post('/store', 'StoreController@store')->name('store');
-        Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-        Route::post('/{store}/update', 'StoreController@update')->name('update');
-        Route::delete('/{store}/destroy', 'StoreController@destroy')->name('destroy');
-    });
-
+    Route::resource('stores', 'StoreController');
     Route::resource('products', 'ProductController');
-
 });
