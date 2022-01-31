1b48f8a6b73fa04119fce01281edb30883698cf4
Erick Firmo <erickfirmo1996@gmail.com>
2022-01-30 23:32:50 -0300

    Update edit stores form

--- resources/views/admin/stores/edit.blade.php
+++ resources/views/admin/stores/edit.blade.php
@@ -24,14 +24,6 @@
         <label for="slug">Slug</label>
         <input id="slug" type="text" name="slug" class="form-control" value="{{ $store->slug }}">
     </div>
-    <div class="form-group">
-        <label for="user">Usuário</label>
-        <select name="user_id" id="user" class="form-control">
-            @foreach ($users as $user)
-                <option @if($user->id == $store->user->id) selected @endif value="{{ $user->id }}">{{ $user->name }}</option>
-            @endforeach
-        </select>
-    </div>
     <div class="mt-3">
         <button type="submit" class="btn btn-lg btn-success">Atualizar Loja</button>
     </div>
