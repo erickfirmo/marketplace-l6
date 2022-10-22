38fa3a82f24743721f3a87ae734bbb58a47b88fd
Erick Firmo <erickfirmo1996@gmail.com>
2022-10-22 18:41:34 -0300

    Update create product form

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -39,17 +39,6 @@
             <span class="invalid-feedback">{{ $message }}</span>
         @enderror
     </div>
-    <div class="form-group">
-        <label for="store">Loja</label>
-        <select name="store_id" id="store" class="form-control @error('store_id') is-invalid @enderror">
-            @foreach ($stores as $store)
-                <option value="{{ $store->id }}">{{ $store->name }} </option>
-            @endforeach
-        </select>
-        @error('store_id')
-            <span class="invalid-feedback">{{ $message }}</span>
-        @enderror
-    </div>
     <div class="mt-3">
         <button type="submit" class="btn btn-lg btn-success">Criar Produto</button>
     </div>
