9110d48a1e8ae1211847d78e5b03c4b888adc0fb
Erick Firmo <erickfirmo1996@gmail.com>
2022-10-22 17:02:16 -0300

    Update navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('home') }}">Marketplace L6</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 @auth
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
