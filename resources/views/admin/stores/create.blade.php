948d3120fe26884d42963a4205ca9d66f8f6d573
Erick Firmo <erickfirmo1996@gmail.com>
2022-10-23 16:50:18 -0300

    Update stores form

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -4,38 +4,7 @@
 <h1>Criar Loja</h1>
 <form action="{{ route('admin.stores.store') }}" method="post">
     @csrf
-    <div class="form-group">
-        <label for="name">Nome da Loja</label>
-        <input id="name" type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
-        @error('name')
-            <span class="invalid-feedback">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="description">Descrição</label>
-        <input id="description" type="text" name="description" class="form-control @error('description') is-invalid @enderror"  value="{{ old('description') }}">
-        @error('description')
-            <span class="invalid-feedback">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="phone">Telefone</label>
-        <input id="phone" type="text" name="phone" class="form-control @error('phone') is-invalid @enderror"  value="{{ old('phone') }}">
-        @error('phone')
-            <span class="invalid-feedback">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="mobile_phone">Celular/WhatsApp</label>
-        <input id="mobile_phone" type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror"  value="{{ old('mobile_phone') }}">
-        @error('mobile_phone')
-            <span class="invalid-feedback">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="slug">Slug</label>
-        <input id="slug" type="text" name="slug" class="form-control">
-    </div>
+    @include('admin.stores.partial.form')
     <div class="mt-3">
         <button type="submit" class="btn btn-lg btn-success">Criar Loja</button>
     </div>
--- resources/views/admin/stores/edit.blade.php
+++ resources/views/admin/stores/edit.blade.php
@@ -5,26 +5,7 @@
 <form action="{{ route('admin.stores.update', [ 'store' => $store->id ]) }}" method="post">
     @csrf
     @method('PUT')
-    <div class="form-group">
-        <label for="name">Nome da Loja</label>
-        <input id="name" type="text" name="name" class="form-control" value="{{ $store->name }}">
-    </div>
-    <div class="form-group">
-        <label for="description">Descrição</label>
-        <input id="description" type="text" name="description" class="form-control" value="{{ $store->description }}">
-    </div>
-    <div class="form-group">
-        <label for="phone">Telefone</label>
-        <input id="phone" type="text" name="phone" class="form-control" value="{{ $store->phone }}">
-    </div>
-    <div class="form-group">
-        <label for="mobile_phone">Celular/WhatsApp</label>
-        <input id="mobile_phone" type="text" name="mobile_phone" class="form-control" value="{{ $store->mobile_phone }}">
-    </div>
-    <div class="form-group">
-        <label for="slug">Slug</label>
-        <input id="slug" type="text" name="slug" class="form-control" value="{{ $store->slug }}">
-    </div>
+    @include('admin.stores.partial.form')
     <div class="mt-3">
         <button type="submit" class="btn btn-lg btn-success">Atualizar Loja</button>
     </div>
