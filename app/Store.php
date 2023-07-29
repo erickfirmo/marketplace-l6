81a995ccdcd93f6357365b150d91e3365e4805cd
Erick Firmo <erickfirmo1996@gmail.com>
2023-07-28 22:46:08 -0300

    Using sluggable at Store model

--- app/Store.php
+++ app/Store.php
@@ -3,10 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Store extends Model
 {
-    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug'];
+    use HasSlug;
+
+    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug', 'logo'];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+                        ->generateSlugsFrom('name')
+                        ->saveSlugsTo('slug');
+    }
 
     /**
      * Return relationship with User
