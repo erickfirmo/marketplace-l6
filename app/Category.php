dc685c37f317b42bea7fc199c6037c8c90007bf8
Erick Firmo <erickfirmo1996@gmail.com>
2023-07-28 22:45:00 -0300

    Using sluggable at Category model

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
     protected $fillable = ['name', 'description', 'slug'];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+                        ->generateSlugsFrom('name')
+                        ->saveSlugsTo('slug');
+    }
 
     /**
      * Return relationship with Products
