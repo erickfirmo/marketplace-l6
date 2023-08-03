fbc3dc77aae2fb83c1417f58ecdfa2cca5f0dd12
Erick Firmo <erickfirmo1996@gmail.com>
2023-08-03 02:02:04 -0300

    Create cart routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('product/{slug}', 'HomeController@single')->name('product.single');
 
+
+Route::prefix('cart')->name('cart.')->group(function() {
+    Route::get('/', 'CartController@index')->name('index');
+    Route::post('add', 'CartController@add')->name('add');
+});
+
 Route::group(['middleware' => 'auth'], function() {
     Route::prefix('admin')->namespace('Admin')->name('admin.')->group(function() {
         Route::resource('stores', 'StoreController');
