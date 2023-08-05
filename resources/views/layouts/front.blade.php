4039663cd7b577d6a095529791ebd9867f1124e0
Erick Firmo <erickfirmo1996@gmail.com>
2023-08-04 22:48:52 -0300

    feat: Criando item carrinho na navbar

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -8,6 +8,7 @@
     <title>Marketplace L6</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .front.row {
             margin-bottom: 40px;
@@ -30,36 +31,28 @@
             </li>
         </ul>
 
-    @auth
-           <ul class="navbar-nav mr-auto">
-                    <li class="nav-item @if(request()->is('admin/stores*')) active @endif">
-                        <a class="nav-link" href="{{route('admin.stores.index')}}">Lojas <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item @if(request()->is('admin/products*')) active @endif">
-                        <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
-                    </li>
-                    <li class="nav-item @if(request()->is('admin/categories*')) active @endif">
-                        <a class="nav-link" href="{{route('admin.categories.index')}}">Categorias</a>
-                    </li>
-                </ul>
+          
 
-                <div class="my-2 my-lg-0">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <span class="nav-link">{{auth()->user()->name}}</span>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" onclick="event.preventDefault();
-                                                                  document.querySelector('form.logout').submit(); ">Sair</a>
+        <div class="my-2 my-lg-0">
+            <ul class="navbar-nav mr-auto">
+                
 
-                            <form action="{{route('logout')}}" class="logout" method="POST" style="display:none;">
-                                @csrf
-                            </form>
-                        </li>
-                       
-                    </ul>
-                </div>
-        @endauth
+                <li class="nav-item d-flex">
+                    @if(session()->has('cart'))
+                        <a href="{{ route('cart.index') }}" class="nav-link">
+                            <span class="badge badge-danger">{{ count(session()->get('cart')) }}</span>
+                        </a>
+                        <span class="my-auto">
+                            <i class="fa fa-shopping-cart"></i>
+                        </span>
+
+                    @endif
+                </li>
+                
+            </ul>
+        </div>
+
+            
 
     </div>
 </nav>
